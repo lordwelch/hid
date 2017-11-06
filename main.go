498eb465a835f1ecfa24db960fd069d22d9e62ac
lordwelch <timmy@narnian.us>
2017-11-06 15:00:44 -0700

    Added basic dvorak and json keymaps Load keymap files from cfg dir Start work on main loop

--- main.go
+++ main.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"bufio"
 	"encoding/binary"
-	"fmt"
+	"encoding/json"
 	"io"
+	"io/ioutil"
 	"os"
+	"path"
+	"strings"
+
+	"github.com/alexflint/go-arg"
 )
+
+type Key struct {
+	name     rune
+	modifier string
+	decimal  int
+}
+
+type Keys []Key
 
 const (
 	LCTRL byte = 1 << iota
@@ -20,28 +34,76 @@ const (
 
 func Press(press [8]byte, file io.Writer) {
 	binary.Write(file, binary.BigEndian, press[:])
+	binary.Write(file, binary.BigEndian, [8]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00})
+}
+
+func Hold(press [8]byte, file io.Writer) {
+	binary.Write(file, binary.BigEndian, press[:])
 }
 
 func main() {
 	var (
-		press   [8]byte = [8]byte{0x00, 0x00, 0x51, 0x00, 0x00, 0x00, 0x00, 0x00} // down
-		press1  [8]byte = [8]byte{0x00, 0x00, 0x2a, 0x00, 0x00, 0x00, 0x00, 0x00} // backspace
-		press2  [8]byte = [8]byte{0x00, 0x00, 0x17, 0x00, 0x00, 0x00, 0x00, 0x00} // t
-		unpress [8]byte = [8]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
+		args struct {
+			SHORTCUT string   `arg:"-S,help:Keymap cycle shortcut"`
+			ORDER    []string `arg:positional,help:Order of keymaps`
+		}
+		hidg0         *os.File
+		err           error
+		keymapsF      []os.FileInfo
+		keys          map[string]Keys
+		cfgPath       = path.Join(os.Getenv("XDG_CONFIG_HOME"), "hid")
+		stdin         = bufio.NewReader(os.Stdin)
+		currentKeyMap int
+		good          bool
 	)
-
-	file, err := os.OpenFile("/dev/hidg0", os.O_WRONLY, os.ModePerm)
-
-	fmt.Println(err)
-	for j := 1; j <= 1000; j++ {
-		Press(press, file)
-		Press(unpress, file)
-		Press(press1, file)
-		Press(unpress, file)
-		Press(press2, file)
-		Press(unpress, file)
+	arg.MustParse(&args)
+	keymapsF, err = ioutil.ReadDir(cfgPath)
+	if err != nil {
+		panic(err)
 	}
 
-	file.Close()
+	hidg0, err = os.OpenFile("/dev/hidg0", os.O_WRONLY, os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, file := range keymapsF {
+		var (
+			ext string
+		)
+
+		ext = path.Ext(file.Name())
+		if strings.ToLower(ext) == "json" {
+			var (
+				tmp     Keys
+				T       *os.File
+				content []byte
+			)
+			T, err = os.Open(strings.TrimSuffix(file.Name(), ext))
+			if err != nil {
+				panic(err)
+			}
+
+			content, err = ioutil.ReadAll(T)
+			if err != nil {
+				panic(err)
+			}
+
+			json.Unmarshal(content, tmp)
+			keys[strings.TrimSuffix(file.Name(), ext)] = tmp
+			T.Close()
+		}
+	}
+	for good {
+		var r rune
+		r, _, err = stdin.ReadRune()
+		for keys[args.ORDER[currentKeyMap]][r].name != r {
+			Press([8]byte{LCTRL, 0x00, 0x57, 0x00, 0x00, 0x00, 0x00, 0x00}, hidg0)
+			currentKeyMap++
+		}
+
+	}
+
+	hidg0.Close()
 
 }
